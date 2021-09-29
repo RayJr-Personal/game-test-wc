9d724ed4c4360b31e9f1bf864af6ed22b29d8600
MageBruh <raymondstevejr@yahoo.com>
2021-09-29 13:37:53 +1300

    Final update
    Finished on the 24th, was just waiting for a response from an instructor that never came.
    BowlingGame.py and Main.py both now have docstrings, and have associating Pydoc HTML files.
    Added docs folder, which includes the test plan and the summary report.

--- gamefiles/BowlingGame.py
+++ gamefiles/BowlingGame.py
@@ -1,11 +1,27 @@
+"""Module consisting of the educational bowling game's mechanics.
+Author: Unknown
+Date: 25th of September 2021
+"""
+
 class BowlingGame:
+    """This class contains the bowling game mechanics."""
+    
+    
     def __init__(self):
         self.rolls=[]
 
     def roll(self,pins):
+        """Adds a 'roll' in the list of rolls.
+        
+        Keyword arguments:
+        pins -- The number of pins knocked down for one roll.
+        This gets added to the list of rolls.
+        """
         self.rolls.append(pins)
 
     def score(self):
+        """Calculates the total score based on the list of rolls.
+        Returns the calculated sum."""
         result = 0
         rollIndex = 0
         for frameIndex in range(10):
@@ -21,17 +37,42 @@ class BowlingGame:
         return result
 
     def isStrike(self, rollIndex):
+        """Checks whether a specified roll equals 10.
+        
+        Keyword arguments:
+        rollIndex -- index used to get the specific roll score to be compared
+        """
         return self.rolls[rollIndex] == 10
     
     def isSpare(self, rollIndex):
+        """Checks whether two specified rolls equal 10.
+        
+        Keyword arguments:
+        rollIndex -- index used to get the specific roll scores to be compared
+        """        
         return self.rolls[rollIndex]+ self.rolls[rollIndex+1]==10
     
     def strikeScore(self,rollIndex):
+        """Returns the next frame's score plus 10 for scoring a strike.
+        
+        Keyword arguments:
+        rollIndex -- index used to get the specific roll scores to be added
+        """        
         return  10+ self.rolls[rollIndex+1]+ self.rolls[rollIndex+2]
 
     def spareScore(self,rollIndex):
+        """Returns the next frame's first roll score plus 10 for scoring a spare.
+        
+        Keyword arguments:
+        rollIndex -- index used to get the specific roll score to be added
+        """
         return  10+ self.rolls[rollIndex+2]
 
     def frameScore(self, rollIndex):
+        """Returns the score for one frame.
+        
+        Keyword arguments:
+        rollIndex -- index used to get the specific frame score
+        """
         return self.rolls[rollIndex] + self.rolls[rollIndex + 1]
 
--- gamefiles/Main.py
+++ gamefiles/Main.py
@@ -1,21 +1,37 @@
+"""Module for testing BowlingGame.py
+Author: Unknown
+Date: 25th of September 2021
+"""
+
 import unittest
 import BowlingGame
 	
 class TestBowlingGame(unittest.TestCase):
-	
+    """
+    This class is designed to contain and conduct test cases using unittest.
+    
+    """
+    
+    
     def setUp(self):
+        """Creates an instance of BowlingGame for testing."""
         self.game = BowlingGame.BowlingGame()
         
     def testGutterGame(self):
+        """Tests a game where each roll is a gutter ball."""
         for i in range(0, 20):
             self.game.roll(0)
         assert self.game.score()==0
         
     def testAllOnes(self):
+        """Tests a game where each roll scores one pin."""
         self.rollMany(1, 20)
         assert self.game.score()==20
         
     def testOneSpare(self):
+        """Tests if the game implements the score bonus
+        of scoring a spare.
+        """
         self.game.roll(5)
         self.game.roll(5)
         self.game.roll(3)
@@ -23,6 +39,9 @@ class TestBowlingGame(unittest.TestCase):
         assert self.game.score()==16
         
     def testOneStrike(self):
+        """Tests if the game implements the score bonus
+        of scoring a strike.
+        """
         self.game.roll(10)
         self.game.roll(4)
         self.game.roll(3)
@@ -30,16 +49,23 @@ class TestBowlingGame(unittest.TestCase):
         assert  self.game.score()==24
         
     def testPerfectGame(self):
+        """Tests a perfect game."""
         self.rollMany(10,12)
         assert self.game.score()==300
         
-    def testOneSpare(self):
+    def testSpareGame(self):
+        """Tests a game where every frame is a spare."""
         self.rollMany(5,21)
         assert self.game.score()==150
         
     def rollMany(self, pins,rolls):
+        """Fills the list of rolls in BowlingGame.
+        
+        Keyword arguments:
+        pins -- the number of pins knocked down for every single roll
+        rolls -- the amount of rolls to fill the list with (must amount to 10 frames worth of rolls)
+        """
         for i in range(rolls):
             self.game.roll(pins)
 
-
-unittest.main()
+#unittest.main()
